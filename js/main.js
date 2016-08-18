931f9cb7289836484c0e2d876c630e12958e4ad2
Eddy Tecson <eddytecson@gmail.com>
2016-08-18 21:28:55 +0800

    added about.html, contact.html, unfinished cookie

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 $(document).ready(function(){
-	$('#toggle-demo').change(changeBgColor);
+	$('html').load(changeBgColor);
+	$('#toggle-demo').change(changeBgColor());
 	// JQ
   	function showTextArea(){
   		$("textarea").removeClass("hidden");
@@ -10,9 +11,40 @@ $(document).ready(function(){
 			$('body').removeClass('bg-blue');
 			$('body').addClass('bg-red');
 		} else {
-		$('body').removeClass('bg-red');
+			$('body').removeClass('bg-red');
 			$('body').addClass('bg-blue');
 		}
+  	}
+
+  	function setCookie(elementValue) {
+  	    document.cookie = elementValue;
+  	}
+
+  	function getCookie(cname) {
+  	    var name = cname + "=";
+  	    var ca = document.cookie.split(';');
+  	    for(var i = 0; i < ca.length; i++) {
+  	        var c = ca[i];
+  	        while (c.charAt(0) == ' ') {
+  	            c = c.substring(1);
+  	        }
+  	        if (c.indexOf(name) == 0) {
+  	            return c.substring(name.length, c.length);
+  	        }
+  	    }
+  	    return "";
+  	}
+
+  	function checkCookie() {
+  	    var user = getCookie("username");
+  	    if (user != "") {
+  	        alert("Welcome again " + user);
+  	    } else {
+  	        user = prompt("Please enter your name:", "");
+  	        if (user != "" && user != null) {
+  	            setCookie("username", user, 365);
+  	        }
+  	    }
   	}
 });
 
