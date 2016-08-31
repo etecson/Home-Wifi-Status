11a9e68635d4fb59f768cc881b1de068f3afe3d3
Eddy Tecson <eddytecson@gmail.com>
2016-08-31 13:25:12 +0900

    added animation

--- js/main.js
+++ js/main.js
@@ -1,8 +1,42 @@
 
 $(document).ready(function(){
-  $('#toggle-demo').change(changeBgColor);
-  function changeBgColor(){  		
-  	if($(this).prop('checked')){
+  $('#toggle-demo').change(toggleActions);
+  $(".nav a").on("click", function(){
+    $("ul.nav").find(".active").removeClass("active"); 
+    $(this).parent().addClass("active");
+  });
+  $('[data-toggle="tooltip"]').tooltip();
+  
+  // $("ul.dropdown-menu>li").click(function(){
+  //     if (noneSelected()) { 
+  //       $('#toggle-demo').bootstrapToggle('off');
+  //       updateBgColor;
+  //     } else { 
+  //       $('#toggle-demo').bootstrapToggle('on');
+  //       updateBgColor;
+  //     }
+  // });  
+
+  // function noneSelected(){
+  //   var n = true;
+  //   $("ul.dropdown-menu>li").each(function(){
+  //     if ($(this).hasClass("selected")) 
+  //       {return n = false};
+  //   });
+  //   return n;
+  // }
+
+  function toggleActions(){  
+    updateBgColor();  
+    var isToggleOn = $('#toggle-demo').prop('checked');
+    if (!isToggleOn) refreshDropdown();
+;
+  }
+  function refreshDropdown(){
+    $('.selectpicker').selectpicker('deselectAll');
+  }
+  function updateBgColor(){  		
+  	if($('#toggle-demo').prop('checked')){
   		$('body').removeClass('bg-blue');
   		$('body').addClass('bg-red');
   	} else {
@@ -10,5 +44,12 @@ $(document).ready(function(){
   		$('body').addClass('bg-blue');
   	}
   }
-});
+  function toggleOn() {
+    $('#toggle-demo').bootstrapToggle('on')
+  }
+  function toggleOff() {
+    $('#toggle-demo').bootstrapToggle('off')  
+  }
+  
 
+});
